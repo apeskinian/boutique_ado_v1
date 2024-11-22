08eebf83d695234368f9bd00ebff4c5e977cbc4f
Matthew Aitken <matt@itken.net>
2024-11-22 12:14:10 +0000

    Add env.py, hide secret_key, add auto debug

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +23,11 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zwr04$4!#o&^v&lcsf*1s1^vg77u-)7)c2-u$96l^d($(q+1^a'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+development = os.environ.get('SET_DEBUG', False)
+DEBUG = development
 
 ALLOWED_HOSTS = [
     '.ws.codeinstitute-ide.net',
