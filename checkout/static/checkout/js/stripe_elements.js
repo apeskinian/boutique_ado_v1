c90b8e9e9ffc0167d1f5a8f7ae1cab55cf32272b
Matthew Aitken <matt@itken.net>
2024-11-28 13:44:51 +0000

    Add stripe elements

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,27 @@
+var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+var client_secret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripe_public_key);
+var elements = stripe.elements();
+var card = elements.create('card', {
+    style: {
+      base: {
+        iconColor: '#fff',
+        color: '#000',
+        fontWeight: '500',
+        fontFamily: 'Roboto, Open Sans, Segoe UI, sans-serif',
+        fontSize: '16px',
+        fontSmoothing: 'antialiased',
+        ':-webkit-autofill': {
+          color: '#fce883',
+        },
+        '::placeholder': {
+          color: '#87BBFD',
+        },
+      },
+      invalid: {
+        iconColor: '#dc3545',
+        color: '#dc3545',
+      },
+    },
+  });
+card.mount('#card-element');
--- checkout/views.py
+++ checkout/views.py
@@ -11,5 +11,7 @@ def checkout(request):
     order_form = OrderForm()
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51QQ7P3KwFb8zfBaln98yb2yZFR0NEF4g5TJ2gHUFQAME2iLEvr791JgP12orn6UE1wek71rx5AhogIQvqDb7e37s00GHDiA5tL',
+        'client_secret': 'test client secret',
     }
     return render(request, 'checkout/checkout.html', context)
