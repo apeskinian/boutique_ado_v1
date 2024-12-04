7eae8e06644f4aa18a83126f9707a78d36e9a2b5
Matthew Aitken <matt@itken.net>
2024-12-04 10:44:37 +0000

    Add product view, url and template

--- products/forms.py
+++ products/forms.py
@@ -10,7 +10,7 @@ class ProductForm(forms.ModelForm):
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        categories = Categories.objects.all()
+        categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
         self.fields['category'].choices = friendly_names
